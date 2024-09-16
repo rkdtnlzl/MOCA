f7a7c7bd886578531f99aded93559c49c38a6644
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-16 14:05:20 +0900

    Initial Commit

---
+++ MOCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MOCA
+//
+//  Created by 강석호 on 9/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MOCA/MOCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOCAApp.swift
+//  MOCA
+//
+//  Created by 강석호 on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MOCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
