cff2fbb2eee43cd0526d1096aa18e3e31195c7eb
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-16 18:06:04 +0900

    [Feat] ImagePicker 기능 추가

--- MOCA/PostMoca/PostMocaView.swift
+++ MOCA/PostMoca/PostMocaView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct PostMocaView: View {
     
     @State private var selectedDate = Date()
-    @State private var isImagePickerPresented = false
-    @State private var selectedImage: UIImage? = nil
-    @State private var isCamera = false
+    @State private var image: UIImage = UIImage()
+    @State private var showImagePicker: Bool = false
     
     var body: some View {
         VStack {
@@ -25,85 +24,23 @@ struct PostMocaView: View {
             .environment(\.locale, Locale(identifier: "ko_KR"))
             .padding()
             
-            if let image = selectedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .cornerRadius(10)
-                    .padding()
-            }
+            Image(uiImage: image)
+                .resizable()
             
-            HStack {
-                Button(action: {
-                    isCamera = true
-                    isImagePickerPresented = true
-                }) {
-                    Image(systemName: "camera")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .clipShape(Circle())
-                        .shadow(radius: 2)
-                }
-                .padding(.trailing, 10)
-                
-                Button(action: {
-                    isCamera = false
-                    isImagePickerPresented = true
-                }) {
-                    Image(systemName: "photo")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .clipShape(Circle())
-                        .shadow(radius: 2)
-                }
-            }
-            .padding()
+            Button(action: {
+                showImagePicker.toggle()
+            }, label: { Text("이미지 선택") })
+            .buttonStyle(.borderedProminent)
             
             Spacer()
-            
         }
-        .sheet(isPresented: $isImagePickerPresented) {
-            ImagePicker(sourceType: isCamera ? .camera : .photoLibrary, selectedImage: $selectedImage)
-        }
+        .fullScreenCover(isPresented: $showImagePicker, content: {
+            ImagePicker(selectedImage: $image)
+        })
     }
 }
 
-// ImagePicker 구조체로 UIImagePickerController를 SwiftUI에 맞게 구현
-struct ImagePicker: UIViewControllerRepresentable {
-    var sourceType: UIImagePickerController.SourceType
-    @Binding var selectedImage: UIImage?
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = sourceType
-        return picker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[.originalImage] as? UIImage {
-                parent.selectedImage = image
-            }
-            picker.dismiss(animated: true)
-        }
-    }
-}
+
 
 #Preview {
     PostMocaView()
