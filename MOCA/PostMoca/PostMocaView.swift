3e9459a3883c207b615f671b3acc4a07366bdc56
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-17 19:06:25 +0900

    [Feat] 카페 위치 찾기 버튼 추가

--- MOCA/PostMoca/PostMocaView.swift
+++ MOCA/PostMoca/PostMocaView.swift
@@ -14,7 +14,7 @@ struct PostMocaView: View {
     @State private var showImagePicker: Bool = false
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             DatePicker(
                 "날짜 선택",
                 selection: $selectedDate,
@@ -22,25 +22,33 @@ struct PostMocaView: View {
             )
             .datePickerStyle(CompactDatePickerStyle())
             .environment(\.locale, Locale(identifier: "ko_KR"))
-            .padding()
             
-            Image(uiImage: image)
-                .resizable()
+            Text("이미지 선택")
             
             Button(action: {
                 showImagePicker.toggle()
-            }, label: { Text("이미지 선택") })
+            }, label: { Image(systemName: "camera") })
             .buttonStyle(.borderedProminent)
             
+            Image(uiImage: image)
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("카페 위치 찾기")
+            })
+            
             Spacer()
+            
         }
         .fullScreenCover(isPresented: $showImagePicker, content: {
             ImagePicker(selectedImage: $image)
         })
+        .padding()
     }
 }
-
-
 
 #Preview {
     PostMocaView()
