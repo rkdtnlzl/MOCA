03a6db262d75fdda34ca5a88c0405d225ca28f55
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-22 17:12:32 +0900

    [Add] Realm Manager 추가

---
+++ MOCA/Realm/MocaRealmManager.swift
@@ -0,0 +1,24 @@
+//
+//  MocaRealmManager.swift
+//  MOCA
+//
+//  Created by 강석호 on 9/22/24.
+//
+
+import SwiftUI
+import RealmSwift
+
+class MocaRealmManager: ObservableObject {
+    @Published var mocaData: [Moca] = []
+    
+    init() {
+        loadMocaData()
+    }
+    
+    func loadMocaData() {
+        let realm = try! Realm()
+        let results = realm.objects(Moca.self)
+        mocaData = Array(results)
+        print("Loaded Moca data: \(mocaData)")
+    }
+}
