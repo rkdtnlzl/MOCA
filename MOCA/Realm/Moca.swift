2521798e3ae3ba8f7105122a1a7ce6890ac1f188
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-22 17:13:53 +0900

    [Fix] 카공 이미지 Data 옵셔널값으로 변경

--- MOCA/Realm/Moca.swift
+++ MOCA/Realm/Moca.swift
@@ -11,11 +11,11 @@ import RealmSwift
 final class Moca: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var createAt: Date
-    @Persisted var images: RealmSwift.List<Data>
+    @Persisted var images: RealmSwift.List<Data?>
     @Persisted var cafeLocation: String
     @Persisted var todos: RealmSwift.List<Todo>
 
-    convenience init(createAt: Date, images: [Data], cafeLocation: String, todos: [Todo]) {
+    convenience init(createAt: Date, images: [Data?], cafeLocation: String, todos: [Todo]) {
         self.init()
         self.createAt = createAt
         self.cafeLocation = cafeLocation
