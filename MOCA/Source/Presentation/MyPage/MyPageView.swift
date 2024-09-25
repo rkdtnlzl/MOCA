89cac39ab37230abf4f870979b445504b78ee9a9
rkdtnlzl <rkdtnlzl@naver.com>
2024-09-25 23:50:44 +0900

    [Feat] 마이페이지 생성

--- MOCA/Source/Presentation/MyPage/MyPageView.swift
+++ MOCA/Source/Presentation/MyPage/MyPageView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct MyPageView: View {
     var body: some View {
-        Text("마이페이지")
+        List {
+            
+            Section {
+                Text("이용약관")
+                    .font(.pretendardMedium16)
+                Text("문의하기")
+                    .font(.pretendardMedium16)
+            }
+            
+            Section {
+                HStack {
+                    Text("데이터 초기화")
+                        .font(.pretendardMedium16)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .listStyle(.insetGrouped)
     }
 }
 
